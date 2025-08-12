3aac2dac6d93b179eb6df601f74a1ad545e57850
Grayden Smith <grayden@graydens.ca>
2025-08-12 10:40:11 -0400

    Update ListZones.ps1

--- ListZones.ps1
+++ ListZones.ps1
@@ -1,11 +1,11 @@
 param (   
     [Parameter(Mandatory=$true)][String]$awsAccessKeyId,
-    [Parameter(Mandatory=$true)][SecureString]$awsSecretAccessKey,
-    [Parameter(Mandatory=$true)][SecureString]$awsSessionToken
+    [Parameter(Mandatory=$true)][String]$awsSecretAccessKey,
+    [Parameter(Mandatory=$true)][String]$awsSessionToken
 )
 
 $env:AWS_ACCESS_KEY_ID = $awsAccessKeyId
-$env:AWS_SECRET_ACCESS_KEY = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($awsSecretAccessKey))
-$env:AWS_SESSION_TOKEN = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($awsSessionToken))
+$env:AWS_SECRET_ACCESS_KEY = $awsSecretAccessKey
+$env:AWS_SESSION_TOKEN = $awsSessionToken
 
 aws route53 list-hosted-zones
